9ce35105cf3a7f1437b766be49b763dfffcb5631
Maria <mariyatkach2002@gmail.com>
2023-08-07 14:55:43 +0300

    setup project

---
+++ craco.config.js
@@ -0,0 +1,17 @@
+const path = require(`path`);
+
+module.exports = {
+  webpack: {
+    alias: {
+      "@components": path.resolve(__dirname, "src/components"),
+      "@constants": path.resolve(__dirname, "src/constants"),
+      "@features": path.resolve(__dirname, "src/features"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@services": path.resolve(__dirname, "src/services"),
+      "@store": path.resolve(__dirname, "src/store"),
+      "@theme": path.resolve(__dirname, "src/theme"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+    },
+  },
+};
