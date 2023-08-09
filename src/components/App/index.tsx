3f05778f800f3d4f336d18044f661615acdb61a3
Maria <mariyatkach2002@gmail.com>
2023-08-09 19:06:23 +0300

    fix: Fix Merge Conflict

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,13 +1,17 @@
 import Layout from "@components/Layout";
+import {Route, Routes} from "react-router-dom";
+import SignUp from "@pages/SignUp";
 
 const App = () => {
-    return(
-      <div>
-          <Layout>
-
-          </Layout>
-      </div>
-    )
+  return(
+    <div>
+      <Layout>
+        <Routes>
+          <Route path="/signup" element={<SignUp/>}/>
+        </Routes>
+      </Layout>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,54 @@
+import {
+  SignupWrapper,
+  StyledCard,
+  StyledCardContainer,
+  StyledDecoration,
+  StyledDescription,
+  StyledInfo,
+  StyledLogo,
+  StyledLogoSpan,
+  StyledTitle
+} from "@pages/SignUp/style";
+import {GhostButton} from "@components/Buttons";
+import RegistrationForm from "@features/registration/components/RegistrationForm";
+import {Link} from "react-router-dom";
+import Layout from "@components/Layout";
+import Logo from "@components/Logo";
+
+
+const SignUp = () => {
+  return(
+      <section>
+        <SignupWrapper>
+          <StyledCard>
+            <StyledCardContainer>
+              <StyledInfo>
+                <Logo/>
+                <StyledTitle>
+                  Welcome to the <StyledLogo>power<StyledLogoSpan>up</StyledLogoSpan></StyledLogo>!
+                </StyledTitle>
+
+                <StyledDescription>
+                  We are the largest society of sport enthusiasts. Here you are sure to find like-minded people! To create an account, choose to register via social network or e-mail.
+                </StyledDescription>
+              </StyledInfo>
+
+              <RegistrationForm/>
+
+              <StyledDecoration>
+                or
+              </StyledDecoration>
+
+              <GhostButton>
+                <Link to="/login">
+                  Log in
+                </Link>
+              </GhostButton>
+            </StyledCardContainer>
+          </StyledCard>
+        </SignupWrapper>
+      </section>
+  )
+}
+
+export default SignUp;
