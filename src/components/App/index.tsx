c90561dc475b9f4efe625e07a1fea4495d3975c1
Maria <mariyatkach2002@gmail.com>
2023-08-14 14:59:18 +0300

    Merge branch development into create-login-page

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,15 +1,17 @@
 import Layout from "@components/Layout";
 import {Route, Routes} from "react-router-dom";
 import SignUp from "@pages/SignUp";
-import {REGISTRATION_CONFIRM_PATH, SIGNUP_PATH, SUCCESS_CONFIRMATION_PATH} from "@constants/paths";
+import {LOGIN_PATH, REGISTRATION_CONFIRM_PATH, SIGNUP_PATH, SUCCESS_CONFIRMATION_PATH} from "@constants/paths";
 import RegistrationConfirm from "@pages/RegistrationConfirm";
 import SuccessConfirmation from "@pages/SuccessConfirmation";
+import {Login} from "@pages/Login";
 
 const App = () => {
   return(
     <div>
       <Layout>
         <Routes>
+          <Route path={LOGIN_PATH} element={<Login/>}/>
           <Route path={SIGNUP_PATH} element={<SignUp/>}/>
           <Route path={REGISTRATION_CONFIRM_PATH} element={<RegistrationConfirm/>}/>
           <Route path={SUCCESS_CONFIRMATION_PATH} element={<SuccessConfirmation/>}/>
