14bec6a63b16aa00ee14ce50e757b3a3e6a5abc0
Maria <mariyatkach2002@gmail.com>
2023-08-19 11:39:27 +0300

    feat: integrate NavigationMenu into HomePage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,10 +7,12 @@ import TabsMenu from "./copmonents/TabsMenu";
 import ArticleList from "./copmonents/ArticleList";
 import { TopTags } from "./copmonents/TopTags";
 import { TopAuthors } from "./copmonents/TopAuthors";
+import NavigationMenu from "@components/NavigationMenu";
 
 const HomePage = () => {
   return (
     <HomePageWrapper>
+      <NavigationMenu/>
 
       <ArticlesWithMenu>
         <TabsMenu />
