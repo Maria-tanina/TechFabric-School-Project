9fcb420d06833416a1803840a601cd5e281018b8
Roman Kremez <>
2023-09-05 17:10:15 +0300

    feat: optimize getData helper

--- src/helpers/getDate.ts
+++ src/helpers/getDate.ts
@@ -1,7 +1,4 @@
 export const getDate = (date: string) => {
   const dateObject = new Date(date);
-  const month = dateObject.toLocaleString("en-US", { month: "long" });
-  const day = dateObject.getDate();
-  const year = dateObject.getFullYear();
-  return `${month} ${day}, ${year}`;
+  return dateObject.toLocaleString("en-US", { year: 'numeric', month: 'long', day: 'numeric' });
 };
