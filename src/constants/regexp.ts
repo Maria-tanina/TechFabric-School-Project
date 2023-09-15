c4b70e2e390082e94f7bf65bb4a4da976fbc2086
Maria <mariyatkach2002@gmail.com>
2023-09-15 13:19:08 +0300

    feat: fix contact-us form validation

--- src/constants/regexp.ts
+++ src/constants/regexp.ts
@@ -2,3 +2,5 @@ export const NAME_REGEX = /^[A-Z][a-z]*$/;
 
 export const PASSWORD_REGEX =
   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
+
+export const EMAIL_REGEX = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
