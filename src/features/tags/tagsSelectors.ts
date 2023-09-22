4a9ed5754031f0935cc5e5f7727da3cb22cafa66
Maria <mariyatkach2002@gmail.com>
2023-09-22 19:25:22 +0300

    feat: fix top tags display

--- src/features/tags/tagsSelectors.ts
+++ src/features/tags/tagsSelectors.ts
@@ -5,5 +5,5 @@ const selectAllTags = topsApi.endpoints?.getTopTags.select();
 
 export const selectTags = createSelector(
   selectAllTags,
-  (queryResult) => queryResult.data?.map((tag) => tag.tagName)
+  (queryResult) => queryResult.data?.map((tag) => tag.tagName) || []
 );
