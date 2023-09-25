e2a0daf3135060e70ef24bf535dd661843350cf6
Maria <mariyatkach2002@gmail.com>
2023-09-25 19:20:16 +0300

    feat: fix redux memoization error

--- src/features/authors/authorsSelectors.ts
+++ src/features/authors/authorsSelectors.ts
@@ -1,15 +1,19 @@
-import { topsApi } from "@services/topsApi";
+import { IQueryParams, topsApi } from "@services/topsApi";
 import { createSelector } from "@reduxjs/toolkit";
+import { RootState } from "../../store";
+
+const selectState = (state: RootState) => state;
+const selectParams = (_: any, params: IQueryParams) => params;
 
 export const selectTopAuthors = createSelector(
-  [(state) => state, (_, params) => params],
+  [selectState, selectParams],
   (state, params) => {
     return topsApi.endpoints?.getTopAuthors.select(params)(state)?.data ?? [];
   }
 );
 
 export const selectTopAuthorsError = createSelector(
-  [(state) => state, (_, params) => params],
+  [selectState, selectParams],
   (state, params) => {
     return topsApi.endpoints?.getTopAuthors.select(params)(state)?.isError;
   }
