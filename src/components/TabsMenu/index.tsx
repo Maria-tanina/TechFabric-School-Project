ade81b5680328d5129bd8dd08f1a82e5a819d987
Maria <mariyatkach2002@gmail.com>
2023-10-04 19:58:28 +0300

    feat: fix bug with navigation

--- src/components/TabsMenu/index.tsx
+++ src/components/TabsMenu/index.tsx
@@ -26,7 +26,7 @@ const TabsMenu: FC<ITabsMenuProps> = ({
   const isLogin = useAppSelector(selectIsLogin);
   const types = useAppSelector(selectSportNames);
   useEffect(() => {
-      handleOrderBy(filterTabs[0].orderBy);
+    handleOrderBy(filterTabs[0].orderBy);
     if (handleTypeChange) {
       handleTypeChange({ target: { value: "" } } as SelectChangeEvent<unknown>);
     }
